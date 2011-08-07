6b2bcdcc6911b526ae6b99c40ff7798acd2bf86e
Anthony F <antscape@gmail.com>
2011-08-07 22:34:39 +1000

    node console

--- math.js
+++ math.js
@@ -1253,55 +1253,12 @@ for(; i<len; i++){
 	}
 }
 */
-if(window.onM && typeof window.onM==="function"){
-	onM();
-	onM=null;
-}
+
 })(
 	function(){
 		if(typeof window === 'undefined'){
-			function con(){
-				global.console.log=function(){};
-				function color(str){
-					return '\x1b[1m' + (str || "") + '\x1b[22m';
-				}
-				var stdin = process.openStdin(),
-					stdio = process.binding("stdio"),
-					stderr= process.stderr,
-					stdout= process.stdout;
-
-				var line = "";
-				stderr.write("Javascript-cas (http://aantthony.github.com/javascript-cas/)\n");
-				stderr.write(">> ");
-				stdin.on("data", function (c) {
-					c = c + ""
-					line+=c;
-					switch (line[line.length-1]) {
-						case "\n": case "\r": case "\u0004":
-						line=line.trim();
-						if(line=="exit"){
-							stdout.write("\n");
-							process.exit()
-						}else if(line==="clear"){
-							stdout.write("\x1b[H\x1b[2J");
-						}else if(line){
-							try{
-								stdout.write(M(line).simplify().toStrings()+"\n");
-							}catch(ex){
-								stderr.write(color(ex)+"\n");
-							}
-						}	
-						stderr.write(">> ");
-						line="";
-						break;
-					default:
-						break
-					}
-				});
-			}
-			global.onM=con;
-			return global;
+			return exports;
 		}
-		return window;;
+		return window;
 	}()
 );
