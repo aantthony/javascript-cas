303ebe91d42e21d9262dd83b15bfab5147090992
Anthony <antscape@gmail.com>
2011-08-28 13:25:21 +1000

    ignore dsstore

--- build.sh
+++ build.sh
@@ -1,14 +1,22 @@
 #!/usr/bin/env bash
-if [ ! -d build]; then
+if [ ! -d build ]; then
 	mkdir build
 fi
 
 cat \
-	src/intro.js
-	
-	
-	
-	src/outro.js
+	src/start.js \
+	src/Array.js \
+	src/language.js \
+	src/operators.js \
+	src/M.js \
+	src/M.assume.js \
+	src/M.latex.js \
+	src/M.differentiate.js \
+	src/M.simplify.js \
+	src/M.apply.js \
+	src/M.toLatex.js \
+	src/M.toStrings.js \
+	src/end.js \
 	> build/math.js
 
 uglifyjs build/math.js \
