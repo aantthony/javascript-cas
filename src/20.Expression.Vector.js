2d6be4b7550a069050d61dbbf9fb3efd5f9393ab
Anthony F <nospamant+git@gmail.com>
2012-05-24 21:27:25 +1000

    Vector differentiation. TODO: Bring scalar functions out the front, which would need a new form of the chain rule
    Also:
    - faster dot product (I think)
    - Scalar division for vectors
    - Vector exponentiation (x^2 = |x|^2, x^1 = x, x^3 = (x.x)x)

--- src/20.Expression.Vector.js
+++ src/20.Expression.Vector.js
@@ -9,9 +9,32 @@ Expression.Vector.prototype[','] = function (x) {
 	this[this.length] = x;
 	return this;
 };
+Expression.Vector.prototype.differentiate = function (x) {
+	return Expression.Vector(Array.prototype.map.call(this, function (c) {
+		return c.differentiate(x);
+	}));
+};
+Expression.Vector.prototype.cross = function (x) {
+	if (this.length !== 3 || x.length !== 3) {
+		throw('Cross product only defined for 3D vectors.');
+	}
+	/*
+	i   j    k
+	x   y    z
+	a   b    c
+	
+	= (yc - zb, za - xc, xb - ya)
+	*/
+	
+	return new Expression.Vector([
+		this[1]['*'](x[2])['-'](this[2]['*'](x[1])),
+		this[2]['*'](x[0])['-'](this[0]['*'](x[2])),
+		this[0]['*'](x[1])['-'](this[1]['*'](x[0]))
+	]);
+};
 Expression.Vector.prototype.default = function (x) {
 	var l = this.length;
-	if (x.constructor === Expression.Vector) {
+	if (x instanceof Expression.Vector) {
 		// Dot product
 		if(l !== x.length) {
 			throw('Vector Dimension mismatch.');
@@ -24,16 +47,12 @@ Expression.Vector.prototype.default = function (x) {
 			);
 		}
 		return sum;
-	} else if (x.constructor === Expression.Matrix) {
+	} else if (x instanceof Expression.Matrix) {
 		
-	} else if (x.constructor) {
-		// Scalar Multiplication
-		var i;
-		var n = new Array(l);
-		for (i = 0; i < l; i++) {
-			n[i] = this[i]['*'](x);
-		}
-		return Expression.Vector(n);
+	} else {
+		return Expression.Vector(Array.prototype.map.call(this, function (c) {
+			return c['*'](x);
+		}));
 	}
 };
 Expression.Vector.prototype['*'] = Expression.Vector.prototype.default;
@@ -52,12 +71,35 @@ Expression.Vector.prototype['+'] = function (x, op) {
 Expression.Vector.prototype['-'] = function (x) {
 	return Expression.Vector.prototype.call(this, x, '-');
 };
-Expression.Vector.prototype['/'] = null;
-Expression.Vector.prototype['^'] = function (x) {
+Expression.Vector.prototype['/'] = function (x) {
+	if (x instanceof Expression.Vector) {
+		throw('Vector division not defined');
+	}
+	return Expression.Vector(Array.prototype.map.call(this, function (c) {
+		return c['/'](x);
+	}));
 	
-	throw ('Unimplemented operator: Vector ^');
-	if(x.constructor === this.constructor){
-		
+};
+Expression.Vector.prototype['^'] = function (x) {
+	if(x instanceof Expression.Integer) {
+		if(x.a === 0) {
+			throw('Raised to zero power');
+		}
+		if(x.a === 1) {
+			return this;
+		}
+		if (x.a === 2) {
+			var S = Global.Zero;
+			var i, l = this.length;
+			for (i = 0; i < l; i++) {
+				S = S['+'](this[i]['^'](x));
+			}
+			return S;
+		} else {
+			return this['^'](new Expression.Integer(x.a - 1))['*'](this);
+		}
+	} else if(x instanceof Expression.Rational){
+		return this['^'](x.a)['^'](Global.One['/'](x.b));
 	} else if (x.constructor === Expression.NumericalComplex) {
 		return new Expression.Complex(this.value + x._real, x._imag);
 	} else if(x.constructor === Expression.List.ComplexCartesian) {
