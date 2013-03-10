b6336e76375fc2799647f01257b1f565313a3eb1
Anthony F <nospamant+git@gmail.com>
2013-03-10 23:02:29 +1100

    update console REPL for newer versions of node

--- console.js
+++ console.js
@@ -1,16 +1,15 @@
-#!/usr/bin/env node
+// Simpled REPL for JavaScript-Cas
 
+var M = require("./build/math.js");
 
-var M=require("./build/math.js");
 (function (){
 	
-	function con(){
+	function con (){
 		global.console.log=function(){};
-		function color(str){
+		function color (str){
 			return '\x1b[1m' + (str || "") + '\x1b[22m';
 		}
 		var stdin = process.openStdin(),
-			stdio = process.binding("stdio"),
 			stderr= process.stderr,
 			stdout= process.stdout;
 
@@ -19,19 +18,19 @@ var M=require("./build/math.js");
 		stderr.write(">> ");
 		stdin.on("data", function (c) {
 			c = c + ""
-			line+=c;
+			line += c;
 			switch (line[line.length-1]) {
 				case "\n": case "\r": case "\u0004":
 				line=line.trim();
-				if(line=="exit"){
+				if(line == "exit"){
 					stdout.write("\n");
 					process.exit()
-				}else if(line==="clear"){
+				}else if(line === "clear"){
 					stdout.write("\x1b[H\x1b[2J");
 				}else if(line){
 					try{
 					    var expr = M(line).simplify()
-						stdout.write(expr.toString()+"\n");
+						stdout.write(expr.s('text/latex').s + "\n");
 					}catch(ex){
 						stderr.write(color(ex)+"\n");
 					}
