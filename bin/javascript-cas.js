c7e0d6e3dbbf0bd20f21bee19a36228ad74b10b8
Anthony Foster <git@af.id.au>
2013-08-07 21:06:59 +1000

    fix parsing of decimals, require Global in two places where it was missing (from refactor)

--- bin/javascript-cas.js
+++ bin/javascript-cas.js
@@ -1,45 +1,31 @@
 // Simpled REPL for JavaScript-Cas
 
-var M = require("../");
+var M = require('../'),
+    repl = require('repl');
+
+var last = null;
 
 function REPL() {
-	// global.console.log = function(){};
-	function color (str){
-		return '\x1b[1m' + (str || "") + '\x1b[22m';
-	}
-	var stdin = process.openStdin(),
-		stderr= process.stderr,
-		stdout= process.stdout;
+    repl.start({
+        eval: function (cmd, context, filename, callback) {
+            cmd = cmd.substring(1, cmd.length - 2);
+            if (cmd === '_') {
+                return callback(null, last);
+            }
+            
 
-	var line = "";
-	stderr.write("Javascript-cas (http://aantthony.github.com/javascript-cas/)\n");
-	stderr.write(">> ");
-	stdin.on('data', function (c) {
-		c = c + ""
-		line += c;
-		switch (line[line.length - 1]) {
-			case '\n': case '\r': case '\u0004':
-			line=line.trim();
-			if (line == 'exit'){
-				stdout.write("\n");
-				process.exit()
-			} else if (line === 'clear') {
-				stdout.write("\x1b[H\x1b[2J");
-			} else if (line) {
-				try {
-				    var expr = M(line).simplify()
-					stdout.write(expr.s('text/latex').s + '\n');
-				} catch (ex) {
-					stderr.write(color(ex) + '\n');
-				}
-			}	
-			stderr.write('>> ');
-			line = "";
-			break;
-		default:
-			break
-		}
-	});
+            try {
+                var result = last = M(cmd);
+                callback(null, result.s('text/javascript').s);
+            } catch(ex) {
+                last = null;
+                callback(ex);
+            }
+            
+        },
+        prompt: 'J>'
+    });
+
 }
 
 module.exports = REPL();
